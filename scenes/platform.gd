f7552d5802d867b81e2faf3bf6e42adaf2c33747
Federico Frenguelli <synasius@gmail.com>
2022-03-24 21:06:43 +0100

    Removed comments and eol tabs

--- scenes/platform.gd
+++ scenes/platform.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
 export (int) var velocity = 50
 export (int) var rotation_velocity = 30
+
 onready var position_2d = get_node("Position2D")
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _process(delta):
 	var direction = (self.global_position - self.position_2d.global_position).normalized()
@@ -25,11 +19,8 @@ func _process(delta):
 	if Input.is_action_pressed("platform_rotate_left"):
 		self.rotation_degrees -= rotation_velocity * delta
 	if Input.is_action_pressed("platform_rotate_right"):
-		self.rotation_degrees += rotation_velocity * delta						
+		self.rotation_degrees += rotation_velocity * delta
+		
 		
 func _move_platform(direction, degrees):
 	return direction.rotated(deg2rad(degrees)) * self.velocity * get_process_delta_time()
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
